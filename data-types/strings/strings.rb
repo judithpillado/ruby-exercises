d18687453ca33dc9942d2fb2624110fdb4cbda4e
Judith Pillado <judith.pillado@gmail.com>
2021-02-27 12:37:00 -0600

    Add chomp method, pass test 10

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -75,10 +75,8 @@ class StringTest < Minitest::Test
   end
 
   def test_10
-    skip
     greeting = "Hello!!\n\n"
-    # In place of the line below, call a method to achieve the expected output.
-    actual = greeting._____
+    actual = greeting.chomp
     expected = "Hello!!\n"
 
     assert_equal expected, actual
