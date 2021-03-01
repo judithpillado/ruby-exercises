1bd0d794499e8ea22fed80c2986ff31acd8c21e0
Judith Pillado <judith.pillado@gmail.com>
2021-02-28 18:08:03 -0600

    Add lucky and unlucky, pass test 1

--- data-types/ints_and_floats/ints_and_floats.rb
+++ data-types/ints_and_floats/ints_and_floats.rb
@@ -5,9 +5,7 @@ class IntsAndFloatsTests < Minitest::Test
   def test_1
     lucky = 7
     unlucky = 13
-    # Using the two variables defined above,
-    # add the lucky number and the unlucky number
-    sum = ________
+    sum = lucky + unlucky
     assert_equal 20, sum
   end
 
