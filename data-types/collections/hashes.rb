a457cdc8d5dab7108931388242fee60bfffdc1b3
Judith Pillado <judith.pillado@gmail.com>
2021-03-03 15:00:47 -0600

    Create new hash,
    pass Hashtest 3

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -14,16 +14,14 @@ class HashesTest < Minitest::Test
     assert_equal 0, empty[:not_found]
   end
 
-  # def test_3
-  #   # In the line below, create a hash called ages. The hash should
-  #   # have a key of "ben" with a value of 4 and a key of "kelly" with
-  #   # a value of 6
-  #   ages = ______
-  #   assert_equal 2, ages.length
-  #   assert_equal 4, ages["ben"]
-  #   assert_equal 6, ages["kelly"]
-  # end
-  #
+  def test_3
+    # In the line below, create a hash called ages. The hash should have a key of "ben" with a value of 4 and a key of "kelly" with a value of 6
+    ages = {"ben" => 4, "kelly" => 6}
+    assert_equal 2, ages.length
+    assert_equal 4, ages["ben"]
+    assert_equal 6, ages["kelly"]
+  end
+
   # def test_4
   #   # In the line below, create a hash called ages. The hash should
   #   # have a key of :ben with a value of 4 and a key of :kelly with
