072bf87d6483f5068a454c4b5e9dd9ae6e243f0a
Judith Pillado <judith.pillado@gmail.com>
2021-03-04 18:25:42 -0600

    Pass NestedCollections test 11

--- data-types/collections/nested_collections.rb
+++ data-types/collections/nested_collections.rb
@@ -109,15 +109,11 @@ class NestesdCollectionsTest < Minitest::Test
   end
 
   def test_11
-    skip
-    # HINT: You may find it valuable to break the three_day_forecast variable
-    # into multiple lines to make it more readable
-
+    # Using the three_day_forecast variable, retrieve the expected piece of information
+    # HINT: You may find it valuable to break the three_day_forecast variable into multiple lines to make it more readable
     three_day_forecast = {days: [{high: 70,low: 63,summary: "Mostly Sunny"},{high: 55,low: 47,summary: "Partly Cloudy"},{high: 77,low: 64,summary: "Sunny"}], "date" => "6-21-18",ref_num: 3456789765456787656}
-    # Using the three_day_forecast variable defined above,
-    # retrieve the expected piece of information
 
-    actual = ________
+    actual = three_day_forecast[:ref_num]
     expected = 3456789765456787656
     assert_equal expected, actual
   end
